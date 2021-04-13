d82faaac652ec91a10b536195b55d180b4a298cc
Hao Nguyen <hao.paul.nguyen@gmail.com>
2021-04-12 22:43:41 -0700

    Fix app

--- InP/streamlit_app.py
+++ InP/streamlit_app.py
@@ -139,25 +139,13 @@ st.markdown('****')
 
 df = pd.read_csv('InP/InP_dataset.csv')
 
-st.write(df)
+csv = df.to_csv(index=False)
 
-#Download Button
-
-def get_table_download_link(df):
-    """Generates a link allowing the data in a given panda dataframe to be downloaded
-    in:  dataframe
-    out: href string
-    """
-    csv = df.to_csv(index=True)
-    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    href = f'<a href="data:file/csv;base64,{b64}" download="hot injection.csv">Download csv file</a>'
-    return href
+b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+st.markdown(href, unsafe_allow_html=True)
 
 
-def render_download_link(df):
-        st.markdown(get_table_download_link(df))
-
-render_download_link(df)
 
 
 st.write('Please let Hao know if something needs to be fixed')
