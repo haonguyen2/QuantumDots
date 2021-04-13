7a34a27a3f9c704b0691d28384abe94656df95ec
Hao Nguyen <hao.paul.nguyen@gmail.com>
2021-04-12 22:28:07 -0700

    Fix app

--- InP/streamlit_app.py
+++ InP/streamlit_app.py
@@ -136,8 +136,29 @@ st.write('Current InP dataset:')
 st.write(pd.read_csv('InP/InP_dataset.csv'))
 st.markdown('****')
 
+df = pd.read_csv('InP/InP_dataset.csv')
+
+# #Download Button
+
+# def get_table_download_link(df):
+#     """Generates a link allowing the data in a given panda dataframe to be downloaded
+#     in:  dataframe
+#     out: href string
+#     """
+#     csv = df.to_csv(index=True)
+#     b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+#     href = f'<a href="data:file/csv;base64,{b64}" download="hot injection.csv">Download csv file</a>'
+#     return href
+
+
+# def render_download_link(df):
+#         st.markdown(get_table_download_link(df))
+
+# df = 
+# render_download_link(df)
+
+
 st.write('Please let Hao know if something needs to be fixed')
 st.write('Thank you!')
 
 st.write('Updated 04/08/2021')
-
